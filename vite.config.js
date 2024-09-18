0f324cd5fecd7bccdd1169b94e9ad0469f814316
salimov333 <salimov333@gmail.com>
2024-09-18 06:01:12 +0200

    ready to deploy to vercel

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   console.log("mode", mode);
-
   return {
     plugins: [react()],
-    base: mode === "production" && process.env.REACT_APP_ENV === "github" ? "/e-commerce-site/" : "/",
   }
 })
