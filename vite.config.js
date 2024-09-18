fee6442e57029e2c9fbd29cf47a66558ea82897e
salimov333 <salimov333@gmail.com>
2024-09-18 03:56:45 +0200

    configure the base url in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === "development" ? "/" : "/e-commerce-site"
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === "development" ? "/" : "/e-commerce-site/",
+  }
 })
