d75995d05ee342f284654f3c72108f4f1e00fc4e
salimov333 <salimov333@gmail.com>
2024-09-18 04:29:36 +0200

    Replace BrowserRouter with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProductList from "./pages/ProductList";
 import Cart from "./pages/Cart";
